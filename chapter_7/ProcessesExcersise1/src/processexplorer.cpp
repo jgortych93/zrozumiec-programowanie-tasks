29ffaac147577beaf22e6bd03bda33ee824697eb
jgortych <jgortych93@gmail.com>
2019-11-25 19:36:06 +0100

    Getting process name added.

--- chapter_7/ProcessesExcersise1/src/processexplorer.cpp
+++ chapter_7/ProcessesExcersise1/src/processexplorer.cpp
@@ -1,9 +1,16 @@
 #include "processexplorer.h"
 
 #include <algorithm>
+#include <fstream>
+#include <limits.h>
+#include <stdexcept>
+#include <stdlib.h>
+#include <string>
 
 #define PROC_DIR_PATH "/proc/"
-#define ERROR_MESSAGE "PROC DIR CANNOT BE OPENED"
+#define PROC_NO_ACCESS_MESSAGE "PROC DIR CANNOT BE OPENED"
+#define COMM_FILE_NO_ACCESS_MESSAGE "Comm file cannot be opened"
+
 
 void ProcessExplorer::getProcessesInfo() const
 {
@@ -18,6 +25,7 @@ void ProcessExplorer::getProcessesInfo() const
             {
                 ProcessInfo procInfo;
                 procInfo.pid = entry->d_name;
+                procInfo.name = this->getProcessName( *entry );
 
                 this->displayInfo( procInfo );
             }
@@ -25,18 +33,35 @@ void ProcessExplorer::getProcessesInfo() const
     }
     else
     {
-        cerr<<ERROR_MESSAGE;
+        cerr<<PROC_NO_ACCESS_MESSAGE;
     }
 }
 
 string ProcessExplorer::getProcessName( const struct dirent& entry ) const
 {
-    return entry.d_name;
+    ifstream inputFile;
+    string commFilePath( PROC_DIR_PATH );
+    commFilePath.append( entry.d_name );
+    commFilePath.append( "/comm" );
+    inputFile.open( commFilePath );
+    if( inputFile.is_open() )
+    {
+        string line;
+        getline( inputFile, line );
+        inputFile.close();
+        return line;
+    }
+    else
+    {
+        cerr<<COMM_FILE_NO_ACCESS_MESSAGE<<endl;
+        throw runtime_error( COMM_FILE_NO_ACCESS_MESSAGE );
+    }
 }
 
 void ProcessExplorer::displayInfo( const ProcessInfo& procInfo ) const
 {
     cout<<"Process id: "<<procInfo.pid<<endl;
+    cout<<"\tName: "<<procInfo.name<<endl;
 }
 
 bool ProcessExplorer::isStringNumeric( const string &dirName ) const
--- chapter_7/ProcessesExcersise1/src/processinfo.h
+++ chapter_7/ProcessesExcersise1/src/processinfo.h
@@ -7,6 +7,7 @@ using namespace std;
 
 typedef struct ProcessInfo
 {
+    string name;
     string pid;
 }ProcessInfo;
 
