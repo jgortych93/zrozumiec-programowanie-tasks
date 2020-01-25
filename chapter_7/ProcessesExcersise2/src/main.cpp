2be4c409f01c62b75a57a7edcb0ccbfe0c8d52ab
jgortych <jgortych93@gmail.com>
2020-01-25 16:31:10 +0100

    Main function implementation.

--- chapter_7/ProcessesExcersise2/src/main.cpp
+++ chapter_7/ProcessesExcersise2/src/main.cpp
@@ -1,8 +1,18 @@
+#include "debugger.hpp"
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    cout<<"Please enter input program path: "<<endl;
+    string path;
+    std::getline( cin, path );
+
+    Debugger debugger( path );
+    debugger.startDebugging();
+
     return 0;
 }
