fbf0a3eb82fc965465349ebc736bd598474eecb3
Jakub Gortych <jakub.gortych@huuugegames.com>
2020-03-25 19:50:47 +0100

    Synchronization excercise 1 - non synchronized access to container example added.

--- chapter_9/SychExercise1/SychExercise1/NonSynchronizedAccessExample.cpp
+++ chapter_9/SychExercise1/SychExercise1/NonSynchronizedAccessExample.cpp
@@ -1,6 +1,10 @@
 #include "pch.h"
 #include "NonSynchronizedAccessExample.h"
 
+#include <iostream>
+#include <thread>
+
+uint8_t NonSynchronizedAccessExample::allThreadReadyLock = 0;
 
 NonSynchronizedAccessExample::NonSynchronizedAccessExample()
 {
@@ -15,13 +19,47 @@ NonSynchronizedAccessExample::~NonSynchronizedAccessExample()
 
 void NonSynchronizedAccessExample::RunTest()
 {
+    thread* threads[THREADS_NUM];
+    for (int i = 0; i < THREADS_NUM / 2; ++i)
+    {
+        threads[i] = new thread(&NonSynchronizedAccessExample::InsertToMap, this, (uint8_t)0, (uint8_t)i);
+    }
+    uint8_t j = 0;
+    for (int i = THREADS_NUM / 2; i < THREADS_NUM ; ++i)
+    {
+        threads[i] = new thread(&NonSynchronizedAccessExample::DeleteFromMap, this, j);
+        ++j;
+    }
+
+    for (int i = 0; i < THREADS_NUM; ++i)
+    {
+        threads[i]->join();
+    }
+
+    this->PrintMap();
 }
 
-void NonSynchronizedAccessExample::InsertToMap(const uint8_t & value)
+void NonSynchronizedAccessExample::InsertToMap(const uint8_t& key, const uint8_t& value)
 {
+    pair<uint8_t, uint8_t> keyValuePair = { key, value };
+    ++allThreadReadyLock;
+    while (THREADS_NUM != allThreadReadyLock) {}
+    this->nonSynchronizedMap->insert(keyValuePair);
 }
 
 void NonSynchronizedAccessExample::DeleteFromMap(const uint8_t & key)
 {
+    ++allThreadReadyLock;
+    while (THREADS_NUM != allThreadReadyLock) {}
+    this->nonSynchronizedMap->erase(key);
+}
 
+void NonSynchronizedAccessExample::PrintMap()
+{
+    cout << "Map content: " << endl;
+    map< uint8_t, uint8_t >::iterator it = this->nonSynchronizedMap->begin();
+    for ( it; it != this->nonSynchronizedMap->end(); ++it)
+    {
+        cout << to_string(it->second) << endl;
+    }
 }
--- chapter_9/SychExercise1/SychExercise1/NonSynchronizedAccessExample.h
+++ chapter_9/SychExercise1/SychExercise1/NonSynchronizedAccessExample.h
@@ -3,6 +3,8 @@
 #include <map>
 #include <string>
 
+#define THREADS_NUM 12
+
 using namespace std;
 
 class NonSynchronizedAccessExample
@@ -13,9 +15,12 @@ public:
     void RunTest();
 
 private:
-    void InsertToMap(const uint8_t& value);
+    void InsertToMap(const uint8_t& key, const uint8_t& value);
     void DeleteFromMap(const uint8_t& key);
+    void PrintMap();
 
     map< uint8_t, uint8_t >* nonSynchronizedMap;
+
+    static uint8_t allThreadReadyLock;
 };
 
