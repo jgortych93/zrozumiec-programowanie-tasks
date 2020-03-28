b3818716bf5dedf89cb4430a1c2519e6c88f9b70
Jakub Gortych <jakub.gortych@huuugegames.com>
2020-03-28 15:42:45 +0100

    Second example running added to main function.

--- chapter_9/SychExercise1/SychExercise1/SynchExercise1.cpp
+++ chapter_9/SychExercise1/SychExercise1/SynchExercise1.cpp
@@ -2,13 +2,48 @@
 //
 
 #include "pch.h"
+#include "Account.h"
 #include "NonSynchronizedAccessExample.h"
 
 #include <iostream>
+#include <thread>
+
+using namespace std;
 
 int main()
 {
-    //First example shows that non synchronized operations are untrusty as the result cannot be predicted
-    NonSynchronizedAccessExample firstProblem;
-    firstProblem.RunTest();
+    cout << "Please choose which example to show: " << endl;
+    cout << "First example shows that non synchronized operations are untrusty as the result cannot be predicted" << endl;
+    cout << "Second example shows the risk of thread deadlocking while two mutexes are being locked by different threads in opposite orders" << endl;
+    uint8_t chosenOption = 0U;
+    cin >> chosenOption;
+
+    switch (chosenOption)
+    {
+    case 1:
+    {
+        NonSynchronizedAccessExample firstProblem;
+        firstProblem.RunTest();
+        break;
+    }
+    case 2:
+    {
+        Account firstAccount(30000);
+        Account secondAccount(50000);
+        thread *firstThread = new thread(&Account::TransformAmountTo, &firstAccount, 10000, secondAccount);
+        thread *secondThread = new thread(&Account::TransformAmountTo, &secondAccount, 55000, firstAccount);
+
+        firstThread->join();
+        secondThread->join();
+
+        delete firstThread;
+        delete secondThread;
+        break;
+    }
+    default:
+        cout << "Wrong option." << endl;
+        main();
+        return 0;
+    }
+
 }
